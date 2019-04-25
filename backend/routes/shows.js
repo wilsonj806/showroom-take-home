45aeceea98e5e78438481038a37abdffdcef0d4d
Wilson <wilsonj806@gmail.com>
2019-04-25 15:59:36 -0400

    Add route for POST-ing new shows
    - Add scripting for a POST request to add a new show in:
      backend/routes/shows.js
      - POST request has some BUT NOT ENOUGH validation
    - Add docs/state-management.md
    - Slight change to README.md

--- backend/routes/shows.js
+++ backend/routes/shows.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const User = require('../models/user');
-const Genre = require('../models/genre');
 const Show = require('../models/show');
-const Comment = require('../models/comment');
+
+const { body, validationResult } = require('express-validator/check');
 
 const router = express.Router();
 
@@ -81,6 +80,43 @@ router.get('/unique', async (req, res) => {
       }, []);
     res.json({unique_shows: reduced});
   } catch(error) {
+    res.status(500).send();
+  }
+});
+
+/**
+ * POST new show that a user is watching
+ */
+router.post('/add', [
+  body('title', 'Title must not be empty').exists({checkFalsy: true}),
+  body('img_url', 'Image URL must not be empty').exists({checkFalsy: true}),
+  body('genre_id', 'Genre must not be empty').exists({checkFalsy: true}),
+], async (req, res) => {
+  /**
+   * NOTE: user_id should be surmised from hidden form inputs
+   * TODO: make sure this can accept form data
+   * TODO: add validation to make sure that an entry for that show doesn't exist already
+   */
+  let errors = validationResult(req);
+  const { user_id, genre_id, title, img_url  } = req.body;
+  try {
+    if (!errors.isEmpty()) {
+      res.status(400).json({
+        errors: errors.mapped()
+      }).send();
+    } else {
+      await Show.create({
+        user_id: user_id,
+        genre_id: genre_id,
+        title: title,
+        img_url: img_url
+      });
+      res.status(200).json({
+        msg: `Success, show added. ${title} added to the profile of user with id: ${user_id}`,
+      }).send();
+    }
+  } catch(error) {
+    console.log(error);
     res.status(500).send();
   }
 });
